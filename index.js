62a2c640ea573cf388596e6fe4c7898c2593fdb9
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-13 00:18:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay (players, instrument){
   var array = []
-  for (var i = 0; i <players.length; i++){
+  for (let i = 0; i <players.length; i++){
     array.push(`${players[i]} plays ${insturments[i]}`)
   }
   return array
